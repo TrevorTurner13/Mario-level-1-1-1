ca2dbfbf0b0d9e3dd22267a9684523b020f39ffa
Ahawboldt <122376901+Ahawboldt@users.noreply.github.com>
2023-04-10 19:31:40 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,30 @@
 function love.load()
+    player = {}
+    player.x = 400
+    player.y = 200
+    player.speed = 3
 
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("d") then
+         player.x = player.x + player.speed
+    end
 
+    if love.keyboard.isDown("a") then
+        player.x = player.x - player.speed
+   end
+
+   if love.keyboard.isDown("s") then
+    player.y = player.y + player.speed
+end
+
+if love.keyboard.isDown("w") then
+    player.y = player.y - player.speed
+end
 end
 
 function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 100)
 
 end
