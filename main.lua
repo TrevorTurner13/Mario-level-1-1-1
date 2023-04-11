3c876e70afebd0ddb830ec9d82ee9278ce8c57e7
DevenHyson <119420731+DevenHyson@users.noreply.github.com>
2023-04-11 12:25:46 -0300

    Collision box update

--- main.lua
+++ main.lua
@@ -16,9 +16,15 @@ function love.load()
     player.animations = {}
     player.animations.right = anim8.newAnimation( player.smallMarioGrid( '4-1', 1), 0.1)
     player.animations.jump = anim8.newAnimation( player.smallMarioGrid( '6-1 ', 1), 0.06)
-    
 
     player.anim = player.animations.right
+
+    player.jump_height = -300    -- Whenever the character jumps, he can reach this height.
+	player.gravity = -500        -- Whenever the character falls, he will descend at this rate.
+
+    player.ground = player.y     -- This makes the character land on the plaform.
+
+    player.y_velocity = 0        -- Whenever the character hasn't jumped yet, the Y-Axis velocity is always at 0.
 
     PLAYERHITBOX = {
         x = 0,
@@ -44,24 +50,17 @@ function love.load()
     koopa.animations.right = anim8.newAnimation( player.smallMarioGrid( '4-1', 1), 0.06)
 
     koopa.anim = koopa.animations.right
-	player.jump_height = -300    -- Whenever the character jumps, he can reach this height.
-	player.gravity = -500        -- Whenever the character falls, he will descend at this rate.
+	
 
 end
 
 function love.update(dt)
     isMoving = false
-    
+    isJumping = false
+
     PLAYERHITBOX.x = player.x
     PLAYERHITBOX.y = player.y
-
-    
-    if love.keyboard.isDown("d") then
-         player.x = player.x + player.speed
-         player.anim = player.animations.right
-         isMoving = true
-         isMovingLeft = false
-    end
+        
     if love.keyboard.isDown("d") then  
         player.x = player.x + player.speed     
         player.anim = player.animations.right
@@ -71,46 +70,37 @@ function love.update(dt)
         end
     end
     
-
     if love.keyboard.isDown("a") then
         player.x = player.x - player.speed
         player.anim = player.animations.right
-        isMoving = true
-        isMovingLeft = true
+        if not isJumping then
+            isMoving = true
+            isMovingLeft = true
+        end
    end
 
     if love.keyboard.isDown('space') then
-		if player.y_velocity == 0 then
-			player.y_velocity = player.jump_height
-		end
-	end
+        if player.y_velocity == 0 then
+            player.y_velocity = player.jump_height
+        end
+    end
 
-	if player.y_velocity ~= 0 then
-		player.y = player.y + player.y_velocity * dt
-		player.y_velocity = player.y_velocity - player.gravity * dt
+    if player.y_velocity ~= 0 then
+        player.y = player.y + player.y_velocity * dt
+        player.y_velocity = player.y_velocity - player.gravity * dt
         isJumping = true
-	end
+    end
 
-	if player.y > player.ground then
-		player.y_velocity = 0
-    	player.y = player.ground   
-	end
+    if player.y > player.ground then
+        player.y_velocity = 0
+        player.y = player.ground   
+    end
     
     if isJumping then
         player.anim = player.animations.jump
         player.anim:gotoFrame(1)
     elseif not isMoving and not isJumping then
         player.anim = player.animations.right
-        isMoving = true
-    end
-
-    if love.keyboard.isDown("w") then
-        player.y = player.y - player.speed
-        player.anim = player.animations.right
-        isMoving = true
-    end
-
-    if isMoving == false then
         player.anim:gotoFrame(4)
     end
 
